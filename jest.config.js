d485c79fef8b0d4b15c648df0934a185557d5285
stefdelec <stephane@arianee.org>
2020-02-13 11:36:53 +0100

    test(certificate): test on hash of certificates V1 and V2

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "roots": [
+        "<rootDir>/test",
+    ],
+    "testEnvironment": "jest-environment-node",
+    "transform": {
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    "moduleFileExtensions": [
+        "ts",
+        "tsx",
+        "js",
+        "jsx",
+        "json",
+        "node"
+    ],
+}
