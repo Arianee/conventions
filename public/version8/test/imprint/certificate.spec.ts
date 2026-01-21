919504397d70b8ba1bd90dc7af6b9adfba9510c6
stefdelec <s.delecroix@gmail.com>
2026-01-21 17:41:07 +0100

    Refactor ArianeeProductCertificate-i18n.json to streamline language enum definitions and enhance category/sub-category structure. Updated test to reflect changes in expected hash and content structure.

--- public/version8/test/imprint/certificate.spec.ts
+++ public/version8/test/imprint/certificate.spec.ts
@@ -1,23 +1,24 @@
 import {isObjectMatchingModel} from 'isobjectmatchingmodel';
 import {Arianee, NETWORK} from "@arianee/arianeejs";
 const ArianeeProductCertificate =  require('../../../version8/ArianeeProductCertificate-i18n.json');
-const PreviousVersionArianeeProductCertificate = require('../../../version7/ArianeeProductCertificate-i18n.json');
 
 describe("version 8", () => {
     it('should create the same imprint',async ()=>{
-        const expectedHash = '0xa40830ba988ca7067c89b4f2d935d86b66991ecec0f582054a920b33a4eae8f9'
+        const expectedHash = '0x19f3626f8a85b8d09076d877826f44a1ccce7516c1492eb92a6457cec28c26b0'
         const content = {
             "$schema": "https://cert.arianee.org/version8/ArianeeProductCertificate-i18n.json",
             "name": "Ma super zoé",
             "serialnumber": [],
             "description": "ma description Le Lorem Ips, comme Aldus PageMaker.",
-            "subDescription": ["dress"]
+            "subCategory": "dress",
+            "category": "clothes"
         }
 
         const arianee = await new Arianee().init(NETWORK.arianeeTestnet);
 
         const wallet = arianee.fromRandomMnemonic();
         const hash = await wallet.utils.cert(ArianeeProductCertificate, content);
-         expect(hash).toBe(expectedHash);
+        
+        expect(hash).toBe(expectedHash);
     })
 });
